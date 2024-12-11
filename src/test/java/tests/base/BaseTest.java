33fce6f4eb68d9671f303798adfb1bfcb5b9cdcc
OlegN <nordhagen88@rambler.ru>
2024-12-11 17:39:39 +0300

    Modify method tearDown

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -65,6 +65,8 @@ public class BaseTest {
         if (ITestResult.FAILURE == result.getStatus()){
             AllureUtils.takeScreenshot(driver);
         }
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
